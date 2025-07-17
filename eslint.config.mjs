2c6a6006402084a1979bdbabba49be1dc6c7391e
JesusOfLagos <jesuswrites20043@gmail.com>
2025-07-17 15:43:34 +0100

    fixed eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,28 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+// ];
+
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.config({
+    extends: ['next', 'next/core-web-vitals', 'next/typescript', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended'],
+    rules: {
+      'react/no-unescaped-entities': 'warn',
+      '@next/next/no-page-custom-font': 'warn',
+      '@typescript-eslint/no-unused-expressions': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',  
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+    },
+  }),
+]
 
 export default eslintConfig;
